0a8057e45b94bbc93f3d2e21cb530cbdda879162
solo <solo@ganguo.hk>
2016-10-12 17:52:52 +0800

    register JsonResponse service provider

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Soloslee\JsonResponse\JsonResponseServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Json' => Soloslee\JsonResponse\Facades\JsonResponse::class,
 
     ],
 
