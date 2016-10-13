0734932bad203acf607be27bba21631253e1c1c4
solo <solo@ganguo.hk>
2016-10-13 13:45:43 +0800

    create base model

---
+++ laravel/app/Models/Model.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+abstract class Model extends BaseModel
+{
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
+
+    public static function isExist(array $columns, $id = null)
+    {
+        return static::when($id, function ($q) use ($id) {
+                return $q->where('id', '<>', $id);
+            })
+            ->where($columns)->take(1)->count() > 0;
+    }
+
+    public static function getValue($id, $key)
+    {
+        $row = static::find($id, [$key]);
+        if ($row) {
+            return $row->{$key};
+        }
+    }
+
+    public static function getValues($id, array $keys)
+    {
+        return static::where('id', $id)->first($keys);
+    }
+
+    public static function getKeyValue($key, $value, array $keys)
+    {
+        return static::where($key, $value)->first($keys);
+    }
+
+    public static function updateValue($id, array $columns)
+    {
+        static::where('id', $id)->update($columns);
+    }
+}
