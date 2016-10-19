650283ad81c2f22f40efb2474dafac45be0cce7c
solo <solo@ganguo.hk>
2016-10-19 15:50:35 +0800

    add method updateToken to User model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,7 +2,25 @@
 
 namespace App\Models;
 
+use Redis;
+use ApiToken;
+
 class User extends Model
 {
     protected $table = 'users';
+
+    public static function updateToken($phone)
+    {
+        $apiToken = ApiToken::genToken();
+        $user = static::where('phone', $phone)->first([
+            'id', 'name', 'avatar', 'api_token as old_api_token'
+        ]);
+        static::where('phone', $phone)->update(['api_token' => $apiToken]);
+        $user->api_token = $apiToken;
+        Redis::pipeline()->hdel('api_tokens', $user->old_api_token)
+            ->hset('api_tokens', $apiToken, $user->id)
+            ->execute();
+
+        return $user->toArray();
+    }
 }
