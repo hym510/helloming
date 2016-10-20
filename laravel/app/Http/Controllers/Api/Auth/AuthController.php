bc92ed04070ce287a942d2e3096ea868b266f919
solo <solo@ganguo.hk>
2016-10-20 11:01:59 +0800

    add user sign in api

--- laravel/app/Http/Controllers/Api/Auth/AuthController.php
+++ laravel/app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use Json;
+use Smser;
+use App\Models\User;
+use App\Http\Requests\SigninRequest;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    public function postLogin()
+    public function postSignin(SigninRequest $request)
     {
-        return Json::success();
+        if (! User::isExist(['phone' => $request->phone])) {
+            return Json::error(
+                'A user with the specified mobile phone number was not found.', 213
+            );
+        }
+
+        if (! Smser::verifySmsCode($request->phone, $request->code)) {
+            return Json::error('Invalid SMS code.', 603);
+        }
+
+        return Json::success(User::updateToken($request->phone));
     }
 }
