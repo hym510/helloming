9294f6ee2326babef3398c3a80969e093de83651
solo <solo@ganguo.hk>
2016-10-20 11:43:14 +0800

    add method genToken to SmsCode model

--- laravel/app/Models/SmsCode.php
+++ laravel/app/Models/SmsCode.php
@@ -2,6 +2,12 @@
 
 namespace App\Models;
 
+use Redis;
+
 class SmsCode
 {
+    public static function genCode($phone)
+    {
+        Redis::setex('phone:'.$phone, 600, mt_rand(100000, 999999));
+    }
 }
