7b36e83c9b9c221392fb113a4e2938515d084833
solo <solo@ganguo.hk>
2016-10-20 10:27:11 +0800

    create a new middleware ApiAuthenticate

---
+++ laravel/app/Http/Middleware/ApiAuthenticate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Json;
+use Redis;
+use Closure;
+
+class ApiAuthenticate
+{
+    public function handle($request, Closure $next)
+    {
+        $userId = Redis::hget('auth_tokens', $request->header('AUTHTOKEN'));
+        if (! $userId) {
+            return Json::error('Invalid auth token.', 104);
+        }
+
+        $request->userId = $userId;
+
+        return $next($request);
+    }
+}
