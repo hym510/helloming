64ed6e086dcfa2780872bb0cd7a272585744e258
solo <solo@ganguo.hk>
2016-10-21 14:32:29 +0800

    changes the structure of users table

--- laravel/database/migrations/2016_10_18_032919_create_tables.php
+++ laravel/database/migrations/2016_10_18_032919_create_tables.php
@@ -15,11 +15,25 @@ class CreateTables extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('phone', 11);
-            $table->string('name', 32);
             $table->string('avatar')->nullable();
+            $table->string('phone', 11);
+            $table->unsignedInteger('experience')->default(0);
+            $table->unsignedInteger('vip_experience')->default(0);
+            $table->unsignedTinyInteger('state')->default(0);
+            $table->string('name', 32);
+            $table->unsignedTinyInteger('height')->default(0);
+            $table->unsignedTinyInteger('weight')->default(0);
+            $table->unsignedTinyInteger('age')->default(0);
             $table->enum('gender', ['male', 'female'])->default('male');
-            $table->unsignedInteger('job_id');
+            $table->unsignedMediumInteger('online_time')->default(0);
+            $table->unsignedSmallInteger('job_id');
+            $table->enum('zodiac', [
+                    'aquarius', 'pisces', 'aries',
+                    'taurus', 'gemini', 'cancer', 'leo', 'virgo',
+                    'libra', 'scorpio', 'sagittarius', 'capricorn'
+                ])->nullable();
+            $table->unsignedSmallInteger('power')->default(0);
+            $table->unsignedSmallInteger('action')->default(0);
             $table->string('api_token', 64);
             $table->string('wechat_id', 64)->nullable();
             $table->timestamp('created_at');
