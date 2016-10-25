b3c4a85e653570d55a7178a728e08ec7f4bd6273
solo <solo@ganguo.hk>
2016-10-25 11:48:33 +0800

    modify method signup in User model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -12,21 +12,26 @@ class User extends Model
     public static function signup(array $data): array
     {
         $authToken = AuthToken::genToken();
-        if (! array_key_exists('avatar', $data)) {
+
+        if (! isset($data['avatar'])) {
             $data['avatar'] = null;
         }
+
         $data['auth_token'] = $authToken;
         $user = static::create($data);
+
         $userArray = static::where('id', $user->id)->first([
                 'id', 'avatar', 'experience', 'vip_experience',
                 'state', 'name', 'height', 'weight', 'gender',
-                'online_time', 'job_id', 'zodiac', 'power',
-                'action'
+                'age', 'online_time', 'job_id', 'zodiac',
+                'power', 'action'
             ])->toArray();
+
         Redis::pipeline()->hset('auth_tokens', $authToken, $user->id)
             ->sadd('phone_numbers', $data['phone'])
             ->hmset('user:'.$user->id, $userArray)
             ->execute();
+
         $userArray['auth_token'] = $authToken;
 
         return $userArray;
@@ -40,11 +45,14 @@ class User extends Model
     public static function updateToken($phone): array
     {
         $apiToken = ApiToken::genToken();
+
         $user = static::where('phone', $phone)->first([
             'id', 'name', 'avatar', 'api_token as old_api_token'
         ]);
-        static::where('phone', $phone)->update(['api_token' => $apiToken]);
         $user->api_token = $apiToken;
+
+        static::where('phone', $phone)->update(['api_token' => $apiToken]);
+
         Redis::pipeline()->hdel('api_tokens', $user->old_api_token)
             ->hset('api_tokens', $apiToken, $user->id)
             ->execute();
@@ -55,12 +63,14 @@ class User extends Model
     public static function updateProfile($id, array $data): array
     {
         static::where('id', $id)->update($data);
+
         $userArray = static::where('id', $id)->first([
                 'avatar', 'experience', 'vip_experience',
                 'state', 'name', 'height', 'weight', 'gender',
                 'online_time', 'job_id', 'zodiac', 'power',
                 'action'
             ])->toArray();
+
         Redis::hmset('user:'.$id, $userArray);
 
         return $userArray;
