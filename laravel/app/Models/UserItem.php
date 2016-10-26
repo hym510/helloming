d30f7ae9db979e83a5661babef888958bffeb475
solo <solo@ganguo.hk>
2016-10-26 14:27:20 +0800

    add method getAll to UserItem model

--- laravel/app/Models/UserItem.php
+++ laravel/app/Models/UserItem.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class UserItem extends Model
 {
     protected $table = 'user_items';
+
+    public static function getAll($userId, $type)
+    {
+        return static::join('items', 'user_items.item_id', '=', 'items.id')
+            ->where('user_items.user_id', $userId)
+            ->where('type', $type)
+            ->orderBy('items.priority', 'desc')
+            ->get([
+                'user_items.id', 'items.name', 'items.icon',
+                'items.info', 'user_items.quantity'
+            ]);
+    }
 }
