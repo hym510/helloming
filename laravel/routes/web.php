72c713e681467ba462e6534915462dd6153b9ba5
solo <solo@ganguo.hk>
2016-10-27 16:55:22 +0800

    shorten line

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,14 +12,14 @@
 */
 Route::group(['namespace' => 'Admin'], function () {
     Route::get('auth/login', ['uses' => 'AuthController@getLogin']);
-    Route::post('auth/logint', ['uses' => 'AuthController@postLogin']);
+    Route::post('auth/login', ['uses' => 'AuthController@postLogin']);
 });
 
 Route::group(['namespace' => 'Admin', 'middleware' => 'admin'], function () {
     Route::get('admin/logout', ['uses' => 'AuthController@getLogout']);
     Route::get('admin/home', ['uses' => 'HomeController@getIndex']);
-    Route::get('admin/resetpassword', ['uses' => 'ResetPasswordController@getReset']);
-    Route::put('admin/updatapassword', ['uses' => 'ResetPasswordController@putReset']);
+    Route::get('admin/resetpwd', ['uses' => 'ResetPasswordController@getReset']);
+    Route::put('admin/updatepwd', ['uses' => 'ResetPasswordController@putReset']);
     Route::get('admin/auth', ['uses' => 'AdminController@getIndex']);
     Route::get('admin/edit', ['uses' => 'AdminController@getEdit']);
     Route::post('admin/store', ['uses' => 'AdminController@postStore']);
