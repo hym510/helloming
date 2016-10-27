847b7a899da02206a3b29f6d9fa74ca970106180
solo <solo@ganguo.hk>
2016-10-27 15:53:26 +0800

    add method getAll to Job model

--- laravel/app/Models/Job.php
+++ laravel/app/Models/Job.php
@@ -2,7 +2,14 @@
 
 namespace App\Models;
 
+use Redis;
+
 class Job extends Model
 {
     protected $table = 'jobs';
+
+    public static function getAll(): array
+    {
+        return json_decode(Redis::get('jobs'));
+    }
 }
