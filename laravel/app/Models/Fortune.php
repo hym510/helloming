0ad85b2b0c0ce8d7e17962770d5bebadb8da970e
solo <solo@ganguo.hk>
2016-10-28 16:39:01 +0800

    create Fortune model

---
+++ laravel/app/Models/Fortune.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Fortune extends Model
+{
+    protected $table = 'fortunes';
+}
--- laravel/app/Models/FortuneChest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class FortuneChest extends Model
-{
-    protected $table = 'fortune_chests';
-}
