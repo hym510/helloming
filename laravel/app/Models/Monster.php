18817b93d4d41d8417d0fb6666224cb7ba3c49fb
solo <solo@ganguo.hk>
2016-10-31 14:25:42 +0800

    add method atk to Monster model

--- laravel/app/Models/Monster.php
+++ laravel/app/Models/Monster.php
@@ -5,4 +5,15 @@ namespace App\Models;
 class Monster extends Model
 {
     protected $table = 'monsters';
+
+    public static function atk($id, $atk): bool
+    {
+        $hp = static::getValue($id, 'hp');
+
+        if ($atk >= $hp) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
