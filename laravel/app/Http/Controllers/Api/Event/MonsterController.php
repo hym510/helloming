494b328ab72bfa61af2966090a940785911622ab
solo <solo@ganguo.hk>
2016-11-01 14:07:00 +0800

    add attack monster api

---
+++ laravel/app/Http/Controllers/Api/Event/MonsterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Event;
+
+use Json;
+use Illuminate\Http\Request;
+use App\Library\Event\Monster;
+use App\Http\Controllers\Controller;
+
+class MonsterController extends Controller
+{
+    public function getAtk(Monster $monster, Request $request, $eventId, $atk)
+    {
+        $success = $monster->atk($eventId, $atk, $request->userId);
+
+        if (! $success) {
+            return Json::error('Lack of physical strength.', 501);
+        }
+
+        return Json::success($success);
+    }
+}
