1d89d733b092b0b8309cf033aaf7f7950d07583c
solo <solo@ganguo.hk>
2016-11-01 17:55:07 +0800

    add method start to HostMining model

--- laravel/app/Models/HostMining.php
+++ laravel/app/Models/HostMining.php
@@ -5,4 +5,10 @@ namespace App\Models;
 class HostMining extends Model
 {
     protected $table = 'host_mining';
+
+    public static function start(array $data)
+    {
+        $data['created_at'] = date('Y-m-d H:i:s');
+        static::create($data);
+    }
 }
