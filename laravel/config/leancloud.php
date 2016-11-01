969c899fc89e02bd1bb3bc56b009b54cf874cdb5
solo <solo@ganguo.hk>
2016-11-01 10:40:23 +0800

    add a config file

---
+++ laravel/config/leancloud.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'app_id' => env('LEANCLOUD_APP_ID'),
+    'app_key' => env('LEANCLOUD_APP_KEY'),
+    'master_key' => env('LEANCLOUD_MASTER_KEY'),
+    'prod' => env('LEANCLOUD_PROD'),
+    'sound' => env('LEANCLOUD_SOUND'),
+    'url' => env('LEANCLOUD_URL'),
+
+];
