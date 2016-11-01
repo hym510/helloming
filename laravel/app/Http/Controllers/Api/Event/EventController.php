02df1ff54b9bcf4e7a27285afeac92b7a2c8d9a0
solo <solo@ganguo.hk>
2016-11-01 10:50:46 +0800

    update get events api

--- laravel/app/Http/Controllers/Api/Event/EventController.php
+++ laravel/app/Http/Controllers/Api/Event/EventController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers\Api\Event;
 
+use Json;
+use Redis;
 use App\Models\Event;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class EventController extends Controller
 {
-    public function getIndex()
+    public function getIndex(Request $request)
     {
-        return Json::success(Event::random());
+        return Json::success(Event::random(
+            Redis::hget('user:'.$request->userId, 'level')
+        ));
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,6 +16,12 @@ Route::group(['namespace' => 'Api'], function () {
         Route::post('auth/signin', ['uses' => 'AuthController@postSignin']);
         Route::post('auth/signup', ['uses' => 'AuthController@postSignup']);
     });
+
+    Route::group(['namespace' => 'Event', 'middleware' => ['auth.api']], function () {
+        Route::get('event', ['uses' => 'EventController@getIndex']);
+        Route::get('monster/atk/{eventId}/{atk}', ['uses' => 'MonsterController@getAtk']);
+    });
+
     Route::group(['namespace' => 'Profile', 'middleware' => ['auth.api']], function () {
         Route::get('backpack/tool', ['uses' => 'BackpackController@getTool']);
         Route::get('profile/detail', ['uses' => 'ProfileController@getDetail']);
