fe5ee1a230ac1c8672a8d81ff4b4bede1b35fa96
solo <solo@ganguo.hk>
2016-11-02 10:45:21 +0800

    modify method start in HostMining model

--- laravel/app/Models/HostMining.php
+++ laravel/app/Models/HostMining.php
@@ -6,9 +6,11 @@ class HostMining extends Model
 {
     protected $table = 'host_mining';
 
-    public static function start(array $data)
+    public static function start($userId, $eventId, $mineId)
     {
-        $data['created_at'] = date('Y-m-d H:i:s');
-        static::create($data);
+        static::create([
+            'user_id' => $userId, 'event_id' => $eventId,
+            'mine_id' => $mineId, 'created_at' => date('Y-m-d H:i:s')
+        ]);
     }
 }
