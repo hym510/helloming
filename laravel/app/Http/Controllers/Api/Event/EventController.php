fff0b6bb856a7e32cf5c6382c1d6a71480d5b3df
solo <solo@ganguo.hk>
2016-11-02 17:49:16 +0800

    update get events api

--- laravel/app/Http/Controllers/Api/Event/EventController.php
+++ laravel/app/Http/Controllers/Api/Event/EventController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers\Api\Event;
 
 use Json;
-use Redis;
-use App\Models\Event;
+use App\Library\Event\Event;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class EventController extends Controller
 {
-    public function getIndex(Request $request)
+    public function getIndex(Event $event, Request $request)
     {
-        return Json::success(Event::random(
-            Redis::hget('user:'.$request->userId, 'level')
-        ));
+        return Json::success($event->random($request->userId));
     }
 }
