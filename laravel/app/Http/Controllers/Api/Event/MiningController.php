0d84b99567fb44e313f1133702dd7649478120de
solo <solo@ganguo.hk>
2016-11-02 13:48:32 +0800

    add start mining api

---
+++ laravel/app/Http/Controllers/Api/Event/MiningController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Event;
+
+use Json;
+use Illuminate\Http\Request;
+use App\Library\Event\Mining;
+use App\Http\Controllers\Controller;
+
+class MiningController extends Controller
+{
+    public function getStart(Mining $mining, Request $request, $eventId)
+    {
+        $success = $mining->start($eventId, $request->userId);
+
+        if (! $success) {
+            return Json::error('Not enough space available.', 401);
+        }
+
+        return Json::success();
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,6 +19,7 @@ Route::group(['namespace' => 'Api'], function () {
 
     Route::group(['namespace' => 'Event', 'middleware' => ['auth.api']], function () {
         Route::get('event', ['uses' => 'EventController@getIndex']);
+        Route::get('mining/start/{eventId}', ['uses' => 'MiningController@getStart']);
         Route::get('monster/atk/{eventId}/{atk}', ['uses' => 'MonsterController@getAtk']);
     });
 
