56624cfd7e75dfe6ac73efc16e38788bcbe6ef79
solo <solo@ganguo.hk>
2016-11-02 14:23:18 +0800

    modify method random in Event model

--- laravel/app/Models/Event.php
+++ laravel/app/Models/Event.php
@@ -13,25 +13,14 @@ class Event extends Model
 
     public static function random($level): array
     {
-        $events = static::where('unlock_level', $level)
+        return static::where('unlock_level', $level)
             ->orderByRaw('RAND()')
             ->limit(6)
             ->get([
                 'id', 'type', 'level',
-                'monster_id', 'mine_id', 'info'
+                'mine_id', 'monster_id',
+                'fortune_id', 'info'
             ])
             ->toArray();
-
-        foreach ($events as &$event) {
-            if ($event['type'] == 'monster') {
-                $event['monster'] = Monster::find($event['monster_id'])->toArray();
-                unset($event['mine_id']);
-            } elseif ($event['type'] == 'mine') {
-                $event['mine'] = Mine::find($event['mine_id'])->toArray();
-                unset($event['monster_id']);
-            }
-        }
-
-        return $events;
     }
 }
