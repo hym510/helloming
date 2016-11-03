6d5d919b7bc2f955d39e42bab50a3153697a1b20
solo <solo@ganguo.hk>
2016-11-03 14:41:31 +0800

    add method getPrizeAttribute to Fortune model

--- laravel/app/Models/Fortune.php
+++ laravel/app/Models/Fortune.php
@@ -5,4 +5,9 @@ namespace App\Models;
 class Fortune extends Model
 {
     protected $table = 'fortunes';
+
+    public function getPrizeAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
