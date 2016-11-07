06301594c9b0964b6b8f033324ab4d9d1aa85ed9
solo <solo@ganguo.hk>
2016-11-07 15:48:25 +0800

    modify open chest api

--- laravel/app/Http/Controllers/Api/Event/ChestController.php
+++ laravel/app/Http/Controllers/Api/Event/ChestController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api\Event;
 
+use Auth;
 use Json;
-use Illuminate\Http\Request;
 use App\Library\Event\Chest;
 use Illuminate\Routing\Controller;
 
 class ChestController extends Controller
 {
-    public function getOpen(Chest $chest, Request $request, $eventId)
+    public function getOpen(Chest $chest, $eventId)
     {
-        $success = $chest->open($eventId, $request->userId);
+        $success = $chest->open($eventId, Auth::user()->user);
 
         if (! $success) {
             return Json::error('Lack of material resources.', 501);
