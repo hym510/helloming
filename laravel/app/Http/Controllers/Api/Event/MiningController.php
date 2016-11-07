a1e122df58011245b8a7d9c71e1494553d397eb5
solo <solo@ganguo.hk>
2016-11-07 15:45:31 +0800

    modify start mining api

--- laravel/app/Http/Controllers/Api/Event/MiningController.php
+++ laravel/app/Http/Controllers/Api/Event/MiningController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api\Event;
 
+use Auth;
 use Json;
-use Illuminate\Http\Request;
 use App\Library\Event\Mining;
 use Illuminate\Routing\Controller;
 
 class MiningController extends Controller
 {
-    public function getStart(Mining $mining, Request $request, $eventId)
+    public function getStart(Mining $mining, $eventId)
     {
-        if (! $mining->start($eventId, $request->userId)) {
+        if (! $mining->start($eventId, Auth::user()->user)) {
             return Json::error('Not enough space available.', 401);
         }
 
