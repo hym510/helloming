57ad6c14f49ad491584104bb4833817d99d34fc6
solo <solo@ganguo.hk>
2016-11-10 10:46:41 +0800

    update admin routes prefix

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -10,13 +10,13 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-Route::group(['namespace' => 'Admin'], function () {
-    Route::get('auth/login', ['uses' => 'AuthController@getLogin']);
-    Route::post('auth/login', ['uses' => 'AuthController@postLogin']);
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    Route::get('login', ['uses' => 'AuthController@getLogin']);
+    Route::post('login', ['uses' => 'AuthController@postLogin']);
 });
 
-Route::group(['namespace' => 'Admin', 'middleware' => 'auth.admin'], function () {
-    Route::get('admin/logout', ['uses' => 'AuthController@getLogout']);
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth.admin'], function () {
+    Route::get('logout', ['uses' => 'AuthController@getLogout']);
     Route::get('admin/home', ['uses' => 'HomeController@getIndex']);
     Route::get('admin/resetpwd', ['uses' => 'ResetPasswordController@getReset']);
     Route::put('admin/updatepwd', ['uses' => 'ResetPasswordController@putReset']);
@@ -27,58 +27,58 @@ Route::group(['namespace' => 'Admin', 'middleware' => 'auth.admin'], function ()
     Route::get('admin/edit/{id}', ['uses' => 'AdminController@getEdit']);
     Route::get('admin/delete/{id}', ['uses' => 'AdminController@getDelete']);
 
-    Route::get('users/', ['uses' => 'UsersController@getIndex']);
+    Route::get('users', ['uses' => 'UsersController@getIndex']);
     Route::get('users/delete/{id}/{type}', ['uses' => 'UsersController@getDelete']);
     Route::get('users/show/{id}', ['uses' => 'UsersController@getShow']);
     Route::get('users/add', ['uses' => 'UsersController@getAdd']);
     Route::post('users/store', ['uses' => 'UsersController@postStore']);
 
-    Route::get('item/', ['uses' => 'ItemsController@getIndex']);
+    Route::get('item', ['uses' => 'ItemsController@getIndex']);
     Route::get('item/add', ['uses' => 'ItemsController@getAdd']);
     Route::get('item/edit/{id}', ['uses' => 'ItemsController@getEdit']);
     Route::get('item/delete/{id}', ['uses' => 'ItemsController@getDelete']);
     Route::get('item/update/{id}', ['uses' => 'ItemsController@postUpdate']);
     Route::post('item/store', ['uses' => 'ItemsController@postStore']);
 
-    Route::get('msg/', ['uses' => 'PushMsgController@getPushMsg']);
+    Route::get('msg', ['uses' => 'PushMsgController@getPushMsg']);
     Route::post('pushmsg', ['uses' => 'PushMsgController@postPushMsg']);
 
-    Route::get('monsters/', ['uses' => 'MonstersController@getIndex']);
+    Route::get('monsters', ['uses' => 'MonstersController@getIndex']);
     Route::get('monsters/add', ['uses' => 'MonstersController@getAdd']);
     Route::get('monsters/edit/{id}', ['uses' => 'MonstersController@getEdit']);
     Route::post('monsters/store', ['uses' => 'MonstersController@postStore']);
     Route::post('monsters/update/{id}', ['uses' => 'MonstersController@postUpdate']);
     Route::get('monsters/delete/{id}', ['uses' => 'MonstersController@getDelete']);
 
-    Route::get('mines/', ['uses' => 'MinesController@getIndex']);
+    Route::get('mines', ['uses' => 'MinesController@getIndex']);
     Route::get('mines/add', ['uses' => 'MinesController@getAdd']);
     Route::get('mines/edit/{id}', ['uses' => 'MinesController@getEdit']);
     Route::post('mines/store', ['uses' => 'MinesController@postStore']);
     Route::post('mines/update/{id}', ['uses' => 'MinesController@postUpdate']);
     Route::get('mines/delete/{id}', ['uses' => 'MinesController@getDelete']);
 
-    Route::get('chests/', ['uses' => 'ChestsController@getIndex']);
+    Route::get('chests', ['uses' => 'ChestsController@getIndex']);
     Route::get('chests/add', ['uses' => 'ChestsController@getAdd']);
     Route::get('chests/edit/{id}', ['uses' => 'ChestsController@getEdit']);
     Route::post('chests/store', ['uses' => 'ChestsController@postStore']);
     Route::post('chests/update/{id}', ['uses' => 'ChestsController@postUpdate']);
     Route::get('chests/delete/{id}', ['uses' => 'ChestsController@getDelete']);
 
-    Route::get('events/', ['uses' => 'EventsController@getIndex']);
+    Route::get('events', ['uses' => 'EventsController@getIndex']);
     Route::get('events/add', ['uses' => 'EventsController@getAdd']);
     Route::post('events/store', ['uses' => 'EventsController@postStore']);
     Route::post('events/update/{id}', ['uses' => 'EventsController@postUpdate']);
     Route::get('events/edit/{id}', ['uses' => 'EventsController@getEdit']);
     Route::get('events/delete/{id}', ['uses' => 'EventsController@getDelete']);
 
-    Route::get('equipments/', ['uses' => 'EquipmentsController@getIndex']);
+    Route::get('equipments', ['uses' => 'EquipmentsController@getIndex']);
     Route::get('equipments/add', ['uses' => 'EquipmentsController@getAdd']);
     Route::post('equipments/store', ['uses' => 'EquipmentsController@postStore']);
     Route::post('equipments/update/{id}', ['uses' => 'EquipmentsController@postUpdate']);
     Route::get('equipments/edit/{id}', ['uses' => 'EquipmentsController@getEdit']);
     Route::get('equipments/delete/{id}', ['uses' => 'EquipmentsController@getDelete']);
 
-    Route::get('shop/', ['uses' => 'ShopController@getIndex']);
+    Route::get('shop', ['uses' => 'ShopController@getIndex']);
     Route::get('shop/add', ['uses' => 'ShopController@getAdd']);
     Route::get('shop/price/{id}/{value}', ['uses' => 'ShopController@getSetPrice']);
     Route::post('shop/store/{id}', ['uses' => 'ShopController@postStore']);
