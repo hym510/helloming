3fdc7457257aa92f61150a2deed5035843f2aa8e
solo <solo@ganguo.hk>
2016-11-10 16:04:41 +0800

    add upgrade equipment api

---
+++ laravel/app/Http/Controllers/Api/Profile/EquipController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\Profile;
+
+use Auth;
+use Json;
+use App\Library\Profile\Equip;
+use Illuminate\Routing\Controller;
+
+class EquipController extends Controller
+{
+    public function getUpgrade(Equip $equip, $position)
+    {
+        $success = $equip->upgrade(Auth::user()->user, $position);
+
+        switch ($success[0]) {
+            case 'max':
+                return Json::error('Max level already reached.', 215);
+            case 'lack':
+                return Json::error('Lack of material resources.', 501);
+            case 'success':
+                return Json::success($success[1]);
+        }
+    }
+}
