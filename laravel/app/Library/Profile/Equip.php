d6ce564f54cb3e133d5a3d13d510883b8a8f80ed
solo <solo@ganguo.hk>
2016-11-10 15:41:56 +0800

    modify method upgrade in Equip class

--- laravel/app/Library/Profile/Equip.php
+++ laravel/app/Library/Profile/Equip.php
@@ -2,7 +2,7 @@
 
 namespace App\Library\Profile;
 
-use App\Models\{Equipment, User};
+use App\Models\{Equipment, User, UserItem};
 
 class Equip
 {
@@ -16,13 +16,61 @@ class Equip
         );
 
         $equip = Equipment::getKeyValue(
-            [['level', $user[$equipPos]], ['max_level', false],
-            ['job_id', $user['job_id']], ['position', $position]],
-            ['upgrade', 'icon']
+            [['level', $user[$equipPos]],
+             ['job_id', $user['job_id']],
+             ['position', $position]],
+            ['max_level', 'upgrade', 'icon']
         );
 
-        if (! $equip) {
-            return [];
+        if ($equip['max_level'] == true) {
+            return ['max'];
         }
+
+        $enough = 0;
+        $itemIds = array();
+        $count = count($equip['upgrade']);
+
+        foreach ($equip['upgrade'] as $item) {
+            $itemIds[] = $item[0];
+        }
+
+        $items = UserItem::where('user_id', $userId)
+            ->whereIn('item_id', $itemIds)
+            ->get(['item_id', 'quantity']);
+
+        if ($items->count() < $count) {
+            return ['lack'];
+        }
+
+        foreach ($equip['upgrade'] as $upgrade) {
+            $item = $items->where('item_id', $upgrade[0])->first();
+
+            if (! $item) {
+                return [];
+            } elseif($item['quantity'] >= $upgrade[1]) {
+                $enough++;
+            }
+        }
+
+        if ($enough != $count) {
+            return ['lack'];
+        }
+
+        foreach ($equip['upgrade'] as $upgrade) {
+            UserItem::where('user_id', $userId)
+                ->where('item_id', $upgrade[0])
+                ->decrement('quantity', $upgrade[1]);
+        }
+
+        User::equipUpgrade($userId, $equipPos);
+
+        $equip = Equipment::getKeyValue(
+            [['level', $user[$equipPos] + 1],
+             ['job_id', $user['job_id']],
+             ['position', $position]],
+            ['power', 'icon']
+        );
+
+        return ['success', $equip];
     }
 }
