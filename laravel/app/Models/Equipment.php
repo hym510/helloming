4749766e0a4513d4db1daae7909e734078648cc7
solo <solo@ganguo.hk>
2016-11-10 11:43:40 +0800

    add method getUpgradeAttribute to Equipment model

--- laravel/app/Models/Equipment.php
+++ laravel/app/Models/Equipment.php
@@ -5,4 +5,9 @@ namespace App\Models;
 class Equipment extends Model
 {
     protected $table = 'equipment';
+
+    public function getUpgradeAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
