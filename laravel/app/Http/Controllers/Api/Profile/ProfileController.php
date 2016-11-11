3b601225598082c5ed8c6d47dd96739ed345c063
solo <solo@ganguo.hk>
2016-11-11 10:57:45 +0800

    modify update user profile api

--- laravel/app/Http/Controllers/Api/Profile/ProfileController.php
+++ laravel/app/Http/Controllers/Api/Profile/ProfileController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\Api\Profile;
 use Auth;
 use Json;
 use Redis;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Routing\Controller;
+use App\Http\Requests\Api\ProfileRequest;
 
 class ProfileController extends Controller
 {
@@ -15,7 +16,7 @@ class ProfileController extends Controller
         return Json::success(Redis::hgetall('user:'.Auth::user()->user));
     }
 
-    public function postUpdate(Request $request)
+    public function postUpdate(ProfileRequest $request)
     {
         return Json::success(User::updateProfile(Auth::user()->user, $request->input()));
     }
