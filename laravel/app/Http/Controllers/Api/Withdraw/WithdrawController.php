a549630b02a718a564eb51d9d8097bb57b71e65d
solo <solo@ganguo.hk>
2016-11-15 15:22:55 +0800

    modify send redpack api

--- laravel/app/Http/Controllers/Api/Withdraw/WithdrawController.php
+++ laravel/app/Http/Controllers/Api/Withdraw/WithdrawController.php
@@ -12,7 +12,12 @@ class WithdrawController extends Controller
 {
     public function getRedpack($gold)
     {
-        Wechat::sendRedpack(Redis::hget('user:'.Auth::user()->user, 'wechat_id'), $gold);
+        if (Redis::hget('user:'.Auth::user()->user, 'gold') < $gold) {
+            return Json::error('Gold are not enough.', 511);
+        }
+
+        $userId = Auth::user()->user;
+        Wechat::sendRedpack($userId, Redis::hget('user:'.$userId, 'wechat_id'), $gold);
 
         return Json::success();
     }
