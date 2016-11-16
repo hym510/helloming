5da914c75270bedecdbeb0d688c276acd44db4b9
solo <solo@ganguo.hk>
2016-11-16 10:32:00 +0800

    declare method genToken as static

--- laravel/app/Library/Token/AuthToken.php
+++ laravel/app/Library/Token/AuthToken.php
@@ -6,7 +6,7 @@ use App\Contracts\Token\AuthToken as AuthTokenContract;
 
 class AuthToken implements AuthTokenContract
 {
-    public function genToken(): string
+    public static function genToken(): string
     {
         return md5(openssl_random_pseudo_bytes(16).microtime());
     }
