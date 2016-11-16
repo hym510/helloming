b7834806c186fe004bd11718eb6f98dada769356
solo <solo@ganguo.hk>
2016-11-16 10:29:39 +0800

    declare method verifySmsCode as static

--- laravel/app/Library/Smser/Smser.php
+++ laravel/app/Library/Smser/Smser.php
@@ -6,12 +6,12 @@ use App\Contracts\Sms\Smser as SmserContract;
 
 class Smser implements SmserContract
 {
-    public function requestSmsCode($phone): bool
+    public static function requestSmsCode($phone): bool
     {
         return true;
     }
 
-    public function verifySmsCode($phone, $code): bool
+    public static function verifySmsCode($phone, $code): bool
     {
         return true;
     }
