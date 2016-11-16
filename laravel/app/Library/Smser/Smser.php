52afa50b36d354d7ffa80fdc7f295cf9a9030935
solo <solo@ganguo.hk>
2016-11-16 14:50:58 +0800

    modify Smser class

--- laravel/app/Library/Smser/Smser.php
+++ laravel/app/Library/Smser/Smser.php
@@ -2,17 +2,68 @@
 
 namespace App\Library\Smser;
 
+use Config;
+use Exception;
 use App\Contracts\Sms\Smser as SmserContract;
 
 class Smser implements SmserContract
 {
-    public static function requestSmsCode($phone): bool
+    protected $url;
+
+    protected $headers = [];
+
+    public function __construct($master = false)
     {
-        return true;
+        $this->url = Config::get('leancloud.url');
+
+        if ($master) {
+            $appKey = Config::get('leancloud.master_key').',master';
+        } else {
+            $appKey = Config::get('leancloud.app_key');
+        }
+
+        array_push($this->headers,
+            'Content-Type:application/json',
+            'X-LC-Id:'.Config::get('leancloud.app_id'),
+            'X-LC-Key:'.$appKey
+        );
     }
 
-    public static function verifySmsCode($phone, $code): bool
+    public function requestSmsCode($phone): bool
     {
-        return true;
+        $data = json_encode(['mobilePhoneNumber' => $phone]);
+
+        $ch = curl_init($this->url.'/1.1/requestSmsCode');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        try {
+            $result = curl_exec($ch);
+            curl_close($ch);
+        } catch(Exception $e) {
+            return false;
+        }
+
+        return empty(json_decode($result, true));
+    }
+
+    public function verifySmsCode($phone, $code): bool
+    {
+        $ch = curl_init($this->url.'/1.1/verifySmsCode/'.$code.'?mobilePhoneNumber='.$phone);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        try {
+            $result = curl_exec($ch);
+            curl_close($ch);
+        } catch(Exception $e) {
+            return false;
+        }
+
+        return empty(json_decode($result, true));
     }
 }
