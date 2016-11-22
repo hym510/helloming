334b90c468aa5140c83b45f460daa8cb12193fe3
solo <solo@ganguo.hk>
2016-11-22 11:34:44 +0800

    modify method getReset in ResetPasswordController

--- laravel/app/Http/Controllers/Admin/ResetPasswordController.php
+++ laravel/app/Http/Controllers/Admin/ResetPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Auth;
 use Hash;
 use Json;
 use App\Http\Controllers\Controller;
@@ -11,14 +12,14 @@ class ResetPasswordController extends Controller
 {
     public function getReset()
     {
-        $user = auth()->user();
+        $user = Auth::user();
 
         return view('admin.reset.index', compact('user'));
     }
 
     public function putReset(ResetPasswordRequest $request)
     {
-        auth()->user()->update(['password' => Hash::make($request->password)]);
+        Auth::user()->update(['password' => Hash::make($request->password)]);
 
         return Json::success();
     }
