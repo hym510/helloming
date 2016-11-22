988b4ab60b1fd80521585fcbb9d39d95717bf5bb
solo <solo@ganguo.hk>
2016-11-22 14:00:51 +0800

    add method updateCache to LevelController class

--- laravel/app/Http/Controllers/Admin/LevelController.php
+++ laravel/app/Http/Controllers/Admin/LevelController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Redis;
 use App\Models\LevelAttr;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\LevelRequest;
 
 class LevelController extends Controller
 {
+    protected function updateCache()
+    {
+        Redis::set('level_attributes',
+            json_encode(LevelAttr::orderBy('level', 'asc')->get()->toArray())
+        );
+    }
+
     public function getIndex()
     {
         $levels = LevelAttr::orderBy('level', 'asc')->get();
@@ -30,6 +38,7 @@ class LevelController extends Controller
     public function postUpdate(LevelRequest $request, $levelId)
     {
         LevelAttr::where('id', $levelId)->update($request->inputData());
+        $this->updateCache();
 
         return redirect()->action('Admin\LevelController@getIndex');
     }
@@ -37,6 +46,7 @@ class LevelController extends Controller
     public function postStore(LevelRequest $request)
     {
         LevelAttr::create($request->inputData());
+        $this->updateCache();
 
         return redirect()->action('Admin\LevelController@getIndex');
     }
