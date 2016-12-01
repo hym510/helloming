e3868f824d6c47f2e76149466b15a2c7c355e1f7
solo <solo@ganguo.hk>
2016-12-01 16:11:06 +0800

    modify get user profile api

--- laravel/app/Http/Controllers/Api/Profile/ProfileController.php
+++ laravel/app/Http/Controllers/Api/Profile/ProfileController.php
@@ -6,6 +6,7 @@ use Auth;
 use Json;
 use Redis;
 use App\Models\{Job, User};
+use App\Library\Profile\Equip;
 use Illuminate\Routing\Controller;
 use App\Http\Requests\Api\ProfileRequest;
 
@@ -16,6 +17,9 @@ class ProfileController extends Controller
         $profile = Redis::hgetall('user:'.Auth::user()->user);
         $profile['job'] = Job::where('id', $profile['job_id'])->first(['name'])->name;
         $profile['next_level_exp'] = json_decode(Redis::get('level_attributes'))[$profile['level'] - 1]->exp;
+        $profile['pos1_upgrade'] = Equip::material($profile['job_id'], $profile['equipment1_level'], 1);
+        $profile['pos2_upgrade'] = Equip::material($profile['job_id'], $profile['equipment2_level'], 2);
+        $profile['pos3_upgrade'] = Equip::material($profile['job_id'], $profile['equipment3_level'], 3);
 
         return Json::success($profile);
     }
