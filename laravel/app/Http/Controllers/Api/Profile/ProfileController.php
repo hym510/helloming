3156a0b2c9801ba4a2f4046cb5e312ef4a718ede
solo <solo@ganguo.hk>
2016-12-02 14:01:43 +0800

    modify get user profile api

--- laravel/app/Http/Controllers/Api/Profile/ProfileController.php
+++ laravel/app/Http/Controllers/Api/Profile/ProfileController.php
@@ -16,8 +16,8 @@ class ProfileController extends Controller
     {
         $profile = Redis::hgetall('user:'.Auth::user()->user);
         $profile['job'] = Job::where('id', $profile['job_id'])->first(['name'])->name;
-        $profile['next_level_exp'] = LevelAttr::where('level', $profile['level'] + 1)->first('exp')->exp;
-        $profile['next_state_power'] = StateAttr::where('level', $profile['state'] + 1)->first('power')->power;
+        $profile['next_level_exp'] = LevelAttr::where('level', $profile['level'] + 1)->first(['exp'])->exp;
+        $profile['next_state_power'] = StateAttr::where('level', $profile['state'] + 1)->first(['power'])->power;
         $profile['pos1_upgrade'] = Equip::material($profile['job_id'], $profile['equipment1_level'], 1);
         $profile['pos2_upgrade'] = Equip::material($profile['job_id'], $profile['equipment2_level'], 2);
         $profile['pos3_upgrade'] = Equip::material($profile['job_id'], $profile['equipment3_level'], 3);
