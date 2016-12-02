e9db27a8299a62d513cf2aaea26807712d21f2d8
solo <solo@ganguo.hk>
2016-12-02 11:41:14 +0800

    modify get user profile api

--- laravel/app/Http/Controllers/Api/Profile/ProfileController.php
+++ laravel/app/Http/Controllers/Api/Profile/ProfileController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\Api\Profile;
 use Auth;
 use Json;
 use Redis;
-use App\Models\{Job, User};
 use App\Library\Profile\Equip;
 use Illuminate\Routing\Controller;
 use App\Http\Requests\Api\ProfileRequest;
+use App\Models\{Job, LevelAttr, StateAttr, User};
 
 class ProfileController extends Controller
 {
@@ -16,7 +16,8 @@ class ProfileController extends Controller
     {
         $profile = Redis::hgetall('user:'.Auth::user()->user);
         $profile['job'] = Job::where('id', $profile['job_id'])->first(['name'])->name;
-        $profile['next_level_exp'] = json_decode(Redis::get('level_attributes'))[$profile['level'] - 1]->exp;
+        $profile['next_level_exp'] = LevelAttr::where('level', $profile['level'] + 1)->first('exp')->exp;
+        $profile['next_state_power'] = StateAttr::where('level', $profile['state'] + 1)->first('power')->power;
         $profile['pos1_upgrade'] = Equip::material($profile['job_id'], $profile['equipment1_level'], 1);
         $profile['pos2_upgrade'] = Equip::material($profile['job_id'], $profile['equipment2_level'], 2);
         $profile['pos3_upgrade'] = Equip::material($profile['job_id'], $profile['equipment3_level'], 3);
