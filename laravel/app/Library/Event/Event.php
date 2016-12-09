90aae35f68ee41bb4ae18a632e7b7e0c0be030fc
solo <solo@ganguo.hk>
2016-12-09 14:01:52 +0800

    modify method random in Event class

--- laravel/app/Library/Event/Event.php
+++ laravel/app/Library/Event/Event.php
@@ -13,26 +13,6 @@ class Event
             return [];
         }
 
-        $events = Event::random(Redis::hget('user:'.$userId, 'level'), $count);
-
-        foreach ($events as &$event) {
-            if ($event['type'] == 'mine') {
-                $event['mine'] = Mine::find($event['mine_id'])->toArray();
-                unset($event['monster_id']);
-                unset($event['chest_id']);
-            } elseif ($event['type'] == 'monster') {
-                $event['monster'] = Monster::find($event['monster_id'])->toArray();
-                unset($event['mine_id']);
-                unset($event['chest_id']);
-            } elseif ($event['type'] == 'chest') {
-                $event['chest'] = Chest::getKeyValue(
-                    [['id', $event['chest_id']]], ['cost_type', 'cost']
-                );
-                unset($event['mine_id']);
-                unset($event['monster_id']);
-            }
-        }
-
-        return $events;
+        return ['events' => Event::random(Redis::hget('user:'.$userId, 'level'), $count)];
     }
 }
