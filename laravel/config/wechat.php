7ac54a67ccc23600020af1503e4eff47f8a6c7eb
solo <solo@ganguo.hk>
2016-12-22 14:02:57 +0800

    modify wechat config file

--- laravel/config/wechat.php
+++ laravel/config/wechat.php
@@ -4,6 +4,8 @@ return [
 
     'app_id' => env('WECHAT_APP_ID'),
     'app_secret' => env('WECHAT_APP_SECRET'),
+    'cert_path' => env('WECHAT_CERT_PATH'),
+    'key_path' => env('WECHAT_KEY_PATH'),
     'mch_id' => env('WECHAT_MCH_ID'),
     'mch_key' => env('WECHAT_MCH_KEY'),
     'token' => env('WECHAT_TOKEN'),
