33b2e94a5f82da395453c7e01c811d50a6fa7a7b
solo <solo@ganguo.hk>
2016-12-27 11:21:00 +0800

    add method addOne to Wechat model

--- laravel/app/Models/Wechat.php
+++ laravel/app/Models/Wechat.php
@@ -5,4 +5,13 @@ namespace App\Models;
 class Wechat extends Model
 {
     protected $table = 'wechat';
+
+    public static function addOne(string $unionId, string $openId)
+    {
+        $model = new static;
+        $model->union_id = $unionId;
+        $model->open_id = $openId;
+
+        $model->save();
+    }
 }
