deba24aa8c74b76914140e87e65149555abd79ba
solo <solo@ganguo.hk>
2016-12-28 11:57:54 +0800

    add post withdraw password api

--- laravel/app/Http/Controllers/Api/Withdraw/WithdrawController.php
+++ laravel/app/Http/Controllers/Api/Withdraw/WithdrawController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers\Api\Withdraw;
 use Auth;
 use Json;
 use Redis;
+use App\Models\User;
 use App\Library\Withdraw\Withdraw;
 use Illuminate\Routing\Controller;
+use App\Http\Requests\Api\WithdrawRequest;
 
 class WithdrawController extends Controller
 {
+    public function postPassword(WithdrawRequest $request)
+    {
+        User::withdraw(Auth::user()->user, $request->password);
+
+        return Json::success();
+    }
+
     public function getRedpack($gold)
     {
         if (Redis::hget('user:'.Auth::user()->user, 'gold') < $gold) {
