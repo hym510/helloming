f8c98504d2cbaa91b8bc3dcdb29a5bb697cc4b3f
solo <solo@ganguo.hk>
2016-12-30 11:13:04 +0800

    add method all to Event class

--- laravel/app/Library/Event/Event.php
+++ laravel/app/Library/Event/Event.php
@@ -7,25 +7,41 @@ use App\Models\Event as EventModel;
 
 class Event
 {
-    public static function random($userId, $count): array
+    public static function all($userId): array
     {
-        if ($count >= 6) {
-            return [];
+        $data = Redis::pipeline()
+            ->get('user_event:' . $userId)
+            ->get('life_cycle')
+            ->execute();
+        $events = json_decode($date[0]);
+        $length = count($events);
+        $newEvents = array();
+        $now = date('Y-m-d H:i:s');
+
+        for ($i = 0; $i < $length; $i++) {
+            if (($events[$i]['created'] + $date[1]) > $now) {
+                $newEvents[] = $events[$i];
+            }
         }
 
-        return ['events' => EventModel::random(Redis::hget('user:'.$userId, 'level'), $count)];
+        Redis::set('user_event:' . $userId, json_encode($newEvents));
+
+        return $newEvents;
     }
 
     public static function addEvent($userId, $eventId, $longitude, $latitude)
     {
-        $events = json_decode(Redis::get('user_event:' . $userId));
+        $data = Redis::pipeline()
+            ->get('user_event:' . $userId)
+            ->get('life_cycle')
+            ->execute();
+        $events = json_decode($date[0]);
         $length = count($events);
         $newEvents = array();
-        $lifeCycle = Redis::get('life_cycle');
         $now = date('Y-m-d H:i:s');
 
         for ($i = 0; $i < $length; $i++) {
-            if (($events[$i]['created'] + $lifeCycle) > $now) {
+            if (($events[$i]['created'] + $date[1]) > $now) {
                 $newEvents[] = $events[$i];
             }
         }
@@ -35,5 +51,14 @@ class Event
         }
 
         Redis::set('user_event:' . $userId, json_encode($newEvents));
+    }
+
+    public static function random($userId, $count): array
+    {
+        if ($count >= 6) {
+            return [];
+        }
+
+        return ['events' => EventModel::random(Redis::hget('user:'.$userId, 'level'), $count)];
     }
 }
