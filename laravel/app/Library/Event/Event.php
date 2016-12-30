a17c7b86c23ee6cb907f8601d7d215223396b679
solo <solo@ganguo.hk>
2016-12-30 10:34:02 +0800

    add method addEvent to Event class

--- laravel/app/Library/Event/Event.php
+++ laravel/app/Library/Event/Event.php
@@ -15,4 +15,25 @@ class Event
 
         return ['events' => EventModel::random(Redis::hget('user:'.$userId, 'level'), $count)];
     }
+
+    public static function addEvent($userId, $eventId, $longitude, $latitude)
+    {
+        $events = json_decode(Redis::get('user_event:' . $userId));
+        $length = count($events);
+        $newEvents = array();
+        $lifeCycle = Redis::get('life_cycle');
+        $now = date('Y-m-d H:i:s');
+
+        for ($i = 0; $i < $length; $i++) {
+            if (($events[$i]['created'] + $lifeCycle) > $now) {
+                $newEvents[] = $events[$i];
+            }
+        }
+
+        if (count($newEvents) < 30) {
+            $newEvents[] = array($userId, $eventId, $longitude, $latitude, $now);
+        }
+
+        Redis::set('user_event:' . $userId, json_encode($newEvents));
+    }
 }
