512a90c412cb1741d0de9645b00a6c838cd223de
solo <solo@ganguo.hk>
2017-01-03 14:37:08 +0800

    modify replenish action api

--- laravel/app/Http/Controllers/Api/Replenish/ReplenishController.php
+++ laravel/app/Http/Controllers/Api/Replenish/ReplenishController.php
@@ -9,21 +9,21 @@ use Illuminate\Routing\Controller;
 
 class ReplenishController extends Controller
 {
-    public function getAction($gold)
+    public function getAction($quantity)
     {
-        if (User::ReplenishAction(Auth::user()->user, $gold)) {
+        if (User::ReplenishAction(Auth::user()->user, $quantity)) {
             return Json::success();
         } else {
-            return Json::error('Gold are not enough.', 511);
+            return Json::error('Currency are not enough.', 512);
         }
     }
 
-    public function getPower($gold)
+    public function getPower($quantity)
     {
-        if (User::ReplenishPower(Auth::user()->user, $gold)) {
+        if (User::ReplenishPower(Auth::user()->user, $quantity)) {
             return Json::success();
         } else {
-            return Json::error('Gold are not enough.', 601);
+            return Json::error('Currency are not enough.', 512);
         }
     }
 }
