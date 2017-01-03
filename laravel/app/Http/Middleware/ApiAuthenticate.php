ce12f68f0cd0e2b0c79fe629f357a80854e2f628
solo <solo@ganguo.hk>
2017-01-03 10:30:40 +0800

    modify middleware ApiAuthenticate

--- laravel/app/Http/Middleware/ApiAuthenticate.php
+++ laravel/app/Http/Middleware/ApiAuthenticate.php
@@ -15,7 +15,14 @@ class ApiAuthenticate
         $userId = Redis::hget('auth_tokens', $request->header('AUTHTOKEN'));
 
         if (! $userId) {
-            return Json::error('Invalid auth token.', 104);
+            $user = User::where('auth_token', $request->header('AUTHTOKEN'))
+                ->first(['id']);
+
+            if (! $user) {
+                return Json::error('Invalid auth token.', 104);
+            }
+
+            $userId = $user->id;
         }
 
         if (! Redis::hget('user:'.$userId, 'activate')) {
