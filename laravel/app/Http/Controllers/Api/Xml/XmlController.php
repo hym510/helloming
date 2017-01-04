9e08af8a5b854a543b67e38fcc5a50094e845f08
koro <koro@ganguo.hk>
2017-01-04 16:16:28 +0800

    modify xml file

--- laravel/app/Http/Controllers/Api/Xml/XmlController.php
+++ laravel/app/Http/Controllers/Api/Xml/XmlController.php
@@ -11,10 +11,9 @@ class XmlController extends Controller
 {
     public function getDownload($file)
     {
-        // $filedata = XmlManagement::where('xmlname', $file . '.xml')->where('mark', 1)->first();
-        // $filename = explode('.', $filedata['xmlname']);
-        // $file = app()->make('path.public') . '/uploads/' . $filename[0] . '_' . $filedata['version'] . '.xml';
-        $file = app()->make('path.public') . '/uploads/' . $file . '.xml';
+        $filedata = XmlManagement::where('xmlname', $file . '.xml')->where('mark', 1)->first();
+        $filename = explode('.', $filedata['xmlname']);
+        $file = app()->make('path.public') . '/uploads/' . $filename[0] . '_' . $filedata['version'] . '.xml';
 
         if (file_exists($file)) {
             return app(ResponseFactory::class)->download($file);
