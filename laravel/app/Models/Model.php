608e46d14a4d71458c6935bf8a02ca6d9da8497b
solo <solo@ganguo.hk>
2017-01-04 11:26:56 +0800

    modify method isExist in Model class

--- laravel/app/Models/Model.php
+++ laravel/app/Models/Model.php
@@ -10,11 +10,11 @@ abstract class Model extends BaseModel
 
     public $timestamps = false;
 
-    public static function isExist(array $column, $id = null): bool
+    public static function isExist(array $column): bool
     {
-        return static::when($id, function ($q) use ($id) {
-            return $q->where('id', '<>', $id);
-        })->where($column)->take(1)->count() > 0;
+        return static::whereExists(function ($query) use ($column) {
+            $query->where($column)->take(1);
+        })->count() > 0;
     }
 
     public static function getValue($id, $key)
