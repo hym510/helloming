77430c22230074ccc1a3a539bfdbc1ed3100a1b6
solo <solo@ganguo.hk>
2017-01-05 17:12:19 +0800

    modify get gold exchange rate api

--- laravel/app/Http/Controllers/Api/Data/ExchangeController.php
+++ laravel/app/Http/Controllers/Api/Data/ExchangeController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Api\Data;
 
 use Json;
-use Redis;
+use App\Library\Redis\ConfigRedis;
 use Illuminate\Routing\Controller;
 
 class ExchangeController extends Controller
 {
     public function getGold()
     {
-        return Json::success(['exchange' => json_decode(Redis::get('gold_exchange'))]);
+        return Json::success(['exchange' => json_decode(ConfigRedis::get('gold_exchange'))]);
     }
 }
