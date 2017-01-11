6ff235bc0d36590ea89024d3ad478332473c1004
solo <solo@ganguo.hk>
2017-01-11 16:51:54 +0800

    modify method start in HostEvent model

--- laravel/app/Models/HostEvent.php
+++ laravel/app/Models/HostEvent.php
@@ -6,14 +6,14 @@ class HostEvent extends Model
 {
     protected $table = 'host_events';
 
-    public static function start($userId, $eventId, $mineId): array
+    public static function start($userId, $eventId, $mineId)
     {
         $model = static::create([
             'user_id' => $userId, 'event_id' => $eventId,
             'mine_id' => $mineId, 'created_at' => date('Y-m-d H:i:s')
         ]);
 
-        return ['host_event_id' => $model->id];
+        return $model->id;
     }
 
     public static function getMine($userId): array
