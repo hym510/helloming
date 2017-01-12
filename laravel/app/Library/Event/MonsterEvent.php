0804af340fd03b6556eec1b8fded1997cdf4c266
solo <solo@ganguo.hk>
2017-01-12 13:51:44 +0800

    modify method atk in MonsterEvent class

--- laravel/app/Library/Event/MonsterEvent.php
+++ laravel/app/Library/Event/MonsterEvent.php
@@ -2,29 +2,33 @@
 
 namespace App\Library\Event;
 
+use App\Library\Event\Prize;
 use App\Library\Redis\Redis;
-use App\Models\{Event, Monster, User, UserItem};
+use App\Models\{Event, HostEvent, Monster};
 
 class MonsterEvent
 {
-    public static function atk($eventId, $atk, $userId): bool
+    public static function atk($hostEventId, $atk, $userId): bool
     {
-        $event = Event::getKeyValue(
-            [['id', $eventId], ['type', 'monster']],
+        $event = HostEvent::getKeyValue(
+            [['id', $hostEventId], ['user_id', $userId]],
+            ['event_id']
+        );
+
+        $monster = Event::getKeyValue(
+            [['id', $event['event_id']], ['type', 'monster']],
             ['type_id', 'exp', 'prize']
         );
 
-        if (! $event) {
+        if (! $monster) {
             return false;
         }
 
-        if (! Monster::atk($event['type_id'], $userId, $atk)) {
+        if (! Monster::atk($monster['type_id'], $userId, $atk)) {
             return false;
         }
 
-        User::addExp($userId, $event['exp']);
-
-        UserItem::getPrize($event['prize'], $userId);
+        Prize::get($hostEventId, $userId, $monster['exp'], $monster['prize']);
 
         return true;
     }
