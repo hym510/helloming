c0aca80a41a2ef9cbcc0aa6b1dd50ace3aeb8448
solo <solo@ganguo.hk>
2017-01-12 10:43:50 +0800

    add method host to HostEvent model

--- laravel/app/Models/HostEvent.php
+++ laravel/app/Models/HostEvent.php
@@ -6,17 +6,19 @@ class HostEvent extends Model
 {
     protected $table = 'host_events';
 
-    public static function start($userId, $eventId, $mineId)
+    public static function host($userId, $eventId): int
     {
         $model = static::create([
             'user_id' => $userId, 'event_id' => $eventId,
-            'mine_id' => $mineId, 'created_at' => date('Y-m-d H:i:s')
+            'created_at' => date('Y-m-d H:i:s')
         ]);
+
+        User::hostEvent($userId);
 
         return $model->id;
     }
 
-    public static function getMine($userId): array
+    public static function getHost($userId): array
     {
         return static::where('user_id', $userId)
             ->get(['id', 'event_id', 'created_at'])
