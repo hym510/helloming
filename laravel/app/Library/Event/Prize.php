ec48bbac1fed92d8bbd51043e70e71466a2f117a
solo <solo@ganguo.hk>
2017-01-17 11:40:56 +0800

    modify method get in Prize class

--- laravel/app/Library/Event/Prize.php
+++ laravel/app/Library/Event/Prize.php
@@ -8,8 +8,6 @@ class Prize
 {
     public static function get($hostEventId, $userId, $exp, $prize)
     {
-        HostEvent::where('id', $hostEventId)->delete();
-        User::freeSpace($userId);
         User::addExp($userId, $exp);
         UserItem::getPrize($prize, $userId);
     }
