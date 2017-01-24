4677a231a99c04869b4388d6af83f2f68ba809ba
xuyu <xuyu16@gmail.com>
2017-01-24 16:52:16 +0800

    fix admin set gold exchange

--- laravel/app/Http/Controllers/Admin/ExchangeGoldController.php
+++ laravel/app/Http/Controllers/Admin/ExchangeGoldController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Redis;
+use App\Models\Configure;
 use App\Http\Requests\Admin\ExchangeGoldRequest;
 
 class ExchangeGoldController extends Controller
@@ -15,7 +16,18 @@ class ExchangeGoldController extends Controller
     public function postStore(ExchangeGoldRequest $request)
     {
         $data = $request->inputData();
-        Redis::set('gold_exchange', json_encode(['gold' => $data['gold'], 'money' => $data['money']]));
+        $key = 'gold_exchange';
+        $value = json_encode(['gold' => $data['gold'], 'money' => $data['money']]);
+        Redis::set($key, $value);
+        $cfg = Configure::where('key', $key)->first();
+        if ($cfg) {
+            $cfg->update(['value' => $value]);
+        } else {
+            Configure::insert([
+                'key' => $key,
+                'value' => $value,
+            ]);
+        }
 
         return $this->backSuccessMsg('设置成功');
     }
