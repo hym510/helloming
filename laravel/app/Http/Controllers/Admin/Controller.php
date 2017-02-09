20dc928d247a81caaacd620b88332708e85c9bb5
solo <solo@ganguo.hk>
2017-02-09 15:48:02 +0800

    modify method backSuccessMsg in class BaseController

--- laravel/app/Http/Controllers/Admin/Controller.php
+++ laravel/app/Http/Controllers/Admin/Controller.php
@@ -6,10 +6,10 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    protected function backSuccessMsg($success_msg)
+    protected function backSuccessMsg($msg)
     {
         return back()->withInput([
-            'success_msg' => $success_msg,
+            'success_msg' => $msg,
         ]);
     }
 }
