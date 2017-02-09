853050909b18f0e58442ca6d947bfcc24b75686c
solo <solo@ganguo.hk>
2017-02-09 14:07:03 +0800

    modify method getIndex in JobsController

--- laravel/app/Http/Controllers/Admin/JobsController.php
+++ laravel/app/Http/Controllers/Admin/JobsController.php
@@ -8,6 +8,8 @@ class JobsController extends Controller
 {
     public function getIndex()
     {
-        return view('admin.job.index', ['jobs' => Job::paginate()->appends(request()->all())]);
+        return view('admin.job.index', [
+            'jobs' => Job::paginate()->appends(request()->all())
+        ]);
     }
 }
